3d5a0e71ee913519d6cca10c5d9b488340168a05
Cedric Spindler <cedric.spindler@fhnw.ch>
2022-08-27 19:35:30 +0200

    Implement reporting metrics for DHT sensor

--- ingest/uploader/uploader_node_exporter.py
+++ ingest/uploader/uploader_node_exporter.py
@@ -6,6 +6,15 @@ import time
 
 import httpx
 from prometheus_client import CollectorRegistry, Gauge, write_to_textfile
+import RPi.GPIO as GPIO
+import board
+import adafruit_dht
+
+# GPIO pins connecting to DHT sensor
+DHT_VCC_PIN = 26
+'GPIO VCC (3.3V)'
+DHT_DATA_PIN = 19
+'GPIO DHT22 (Data)'
 
 async def get_response(client, target, timeout):
     try:
@@ -37,6 +46,24 @@ def get_mountpoint_state():
     except: return 0
     else: return 1
 
+def get_temp_humi():
+    temp = None
+    humi = None
+    retry = 0
+    dhtDevice = adafruit_dht.DHT22(board.D19, use_pulseio=False)
+    while True:
+        try:
+            temp = dhtDevice.temperature
+            humi = dhtDevice.humidity
+            break
+        except:
+            if retry > 2:
+                break
+            time.sleep(0.05)
+            retry += 1
+            continue
+    dhtDevice.exit()
+    return (temp, humi)
 
 async def test_http_targets(targets, timeout):
     async with httpx.AsyncClient() as client:
@@ -59,6 +86,7 @@ def main():
     parser.add_argument('--interval',     help='Gathering inteval in seconds', default='2')
     parser.add_argument('--mountpoint',   help='External HDD mountpoint to check', default='/mnt/elements')
     parser.add_argument('--http-timeout', help='HTTP connection timeout', default=2)
+    parser.add_argument('--report-dht',   help='Report temperature/humidity from DHT sensor', action='store_true')
     args = parser.parse_args()
 
     # check if output can be written
@@ -77,12 +105,21 @@ def main():
         'node_mountpoint_state': Gauge('node_mountpoint_state', 'Disk mountpoint state (1: mounted and readable)', registry=registry)
     }
 
+    if args.report_dht:
+        collectors['node_env_temp_celsius'] = Gauge('node_env_temp_celsius', 'Environment temperature inside enclosure', registry=registry)
+        collectors['node_env_humid_percent'] = Gauge('node_env_humid_percent', 'Environment humidity inside enclosure', registry=registry)
+        # set up gpio for dht sensor
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(DHT_VCC_PIN, GPIO.OUT)
+        GPIO.output(DHT_VCC_PIN, GPIO.HIGH)
+        GPIO.setup(DHT_DATA_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
     while True:
 
         records = database.execute('select id, url, enabled, status_code from cameras').fetchall()
         targets = [{'name': r[0], 'url': r[1], 'enabled': r[2], 'status': r[3]} for r in records]
-        metrics = asyncio.run(test_http_targets(targets, args.http_timeout))
-        for m in metrics:
+        http_metrics = asyncio.run(test_http_targets(targets, args.http_timeout))
+        for m in http_metrics:
             if m['status'] != None:
                 collectors['cam_response_latency'].labels(m['target']['name']).set(m['elapsed'])
                 collectors['cam_response_code'].labels(m['target']['name']).set(m['target']['status'])
@@ -91,6 +128,11 @@ def main():
                 collectors['cam_reachable'].labels(m['target']['name']).set(0)
 
         collectors['node_mountpoint_state'].set(get_mountpoint_state())
+
+        if args.report_dht:
+            env_metrics = get_temp_humi()
+            if env_metrics[0]: collectors['node_env_temp_celsius'].set(env_metrics[0])
+            if env_metrics[1]: collectors['node_env_humid_percent'].set(env_metrics[1])
 
         write_to_textfile(args.metrics_path, registry)
         time.sleep(5)
