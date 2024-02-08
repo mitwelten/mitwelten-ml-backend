342f26453b364f88354b5fe793061e9ce235e98e
Cedric Spindler <cedric.spindler@fhnw.ch>
2024-02-08 12:09:46 +0100

    Integrate modes `labels` and `confidences` into evaluation

--- evaluation/pollinators/README.md
+++ evaluation/pollinators/README.md
@@ -2,35 +2,71 @@
 
 ## Generating Model Performance Metrics
 
-To generate the model performance metrics, use [`evaluate.py`](./evaluate.py). The script loads the ground truth data from label-studio JSON and the predictions from the database. It then compares the ground truth data (labels created manually in [label-studio](#label-studio-import)) with the predictions, and outputs metrics in CSV format to standard output.
+To generate the model performance metrics, use [`evaluate.py`](./evaluate.py). The script loads the ground truth data from label-studio JSON and the predictions from the database. The script can be run in two modes, `labels` and `confidences`:
+
+`labels` evaluates predictions by comparing the ground truth data (labels created manually in [label-studio](#label-studio-import)) with the predictions, and outputs metrics in CSV format to standard output.
+
+`confidences` evaluates predictions that were manually graded with confidences in label-studio and outputs collecive metrics as well as per class metrics to standard output.
 
 ### Usage
 
 ```bash
-python evaluate.py [user-id] [threshold-flowers] [threshold-pollinators]
+python evaluate.py [mode] -p [project_id] -u [user_id] -tf [threshold_flowers] -tp [threshold_pollinators]
 ```
 
 The script takes the following arguments:
 
-- `user-id`: The user ID of the user who created the ground truth data in label-studio.
-- `threshold-flowers` / `threshold-pollinators`: IoU thresholds for matching the annotation rectangles.
+- `-p PROJECT_ID`: The project ID of the project in label-studio containing the ground truth data.
+- `-u USER_ID`: The user ID of the user who created the ground truth data in label-studio.
+- `-tf THRESHOLD_FLOWERS` and `-tp THRESHOLD_POLLINATORS`: IoU thresholds for matching the annotation rectangles.
+
+### Examples
+
+#### Evaluate predictions with manual labels
+
+Compare ground-truth labels of project 5, user 5, with predictions in the database. Use a IoU threshold of 0.4 for flowers and 0.3 for pollinators.
+
+```bash
+python evaluate.py labels -p 5 -u 5 -tf 0.4 -tp 0.3
+```
+
+#### Evaluate predictions with manual confidences
+
+Evaluate predictions of project 10, user 5, with the manual confidences.
+
+```bash
+python evaluate.py confidences -p 10 -u 5
+```
 
 ### Method
 
+#### Mode `labels`
+
 The rectangles from the ground truth data are compared with the rectangles from
-the database. They matched if they overlap by more than 50% (or
-custom threshold) by the IoU (Intersection over Union) metric, and if they
-have the same class.
+the database. They matched _if they overlap_ by more than 50% (or
+custom threshold `-tf`, `-tp`) by the IoU (Intersection over Union) metric, and _if they have the same class_.
 
 The number of matched rectangles is then compared with the number of rectangles
 in the ground truth data and the number of rectangles in the database, to
 calculate the number of false positives, false negatives, true positives and from those nummbers the precision, recall and F1 score.
+
+#### Mode `confidences`
+
+The manually added confidences are used to classify the predictions into
+categories of false and true positives. Since we only evaluate predictions
+and not the ground truth, we can't calculate the recall and F1 score but we can
+calculate the precision of the model overall and for the specific classes.
+
+In a further step we could compare the manual confidence with the confidence
+of the model to see if the model is overconfident or underconfident.
 
 ### Input/Output
 
 The script takes a JSON file as input (or reads the Label Studio task files from
 S3 storage). The input contains the ground truth data for images. The JSON
 file is expected to be in the folder [`ground_truth`](./ground_truth). The content of the JSON file is expected to contain records in the label-studio task format, containing the `file_id` of the image along with the rectangles and class in the `result` field.
+
+For more details on data format and procedures read the docstrings for the functions `evaluate_with_groundtruth()` and `evaluate_with_confidence()` in [`evaluate.py`](./evaluate.py).
 
 ## Label-Studio Import
 
--- evaluation/pollinators/evaluate.py
+++ evaluation/pollinators/evaluate.py
@@ -15,7 +15,7 @@ This script is used to compare the ground truth data (labels created manually in
 [label studio](label.mitwelten.org)) with the predictions in the database.
 '''
 
-def read_tasks_from_minio():
+def read_tasks_from_minio(cache_file):
     # read all tasks from minio storage
     bucket = 'ixdm-mitwelten-labels'
     s3 = Minio(
@@ -28,12 +28,13 @@ def read_tasks_from_minio():
     tqdm.write('reading tasks from minio storage...')
     for obj in tqdm(object_list):
         data = json.loads(s3.get_object(bucket, obj.object_name).read())
+        # 'file_id' is our custom field, and is the id used in files_images table in the database
         if 'file_id' in data['task']['data']:
             tasks.append(data)
 
     # write tasks to json file
-    os.makedirs('ground_truth', exist_ok=True)
-    with open('ground_truth/tasks.json', 'w') as f:
+    os.makedirs(os.path.dirname(cache_file), exist_ok=True)
+    with open(cache_file, 'w') as f:
         json.dump(tasks, f)
 
 def intersect(x1, y1, w1, h1, x2, y2, w2, h2):
@@ -52,6 +53,8 @@ def iou(x1, y1, w1, h1, x2, y2, w2, h2):
 
 def match_p(gt, p_rects, threshold=0.5):
     '''
+    match prediction
+
     gt: ground truth rectangle
         {'height': 5.387647831800252,
         'rectanglelabels': ['daisy'],
@@ -80,6 +83,7 @@ def match_p(gt, p_rects, threshold=0.5):
     return None
 
 def match_gt(p, gt_rects, threshold=0.5):
+    'match ground truth'
     # 0 confidence, 1 class, 2 x, 3 y, 4 width, 5 height
     for i, gt in enumerate(gt_rects):
         # match class
@@ -111,39 +115,36 @@ def match_all(gt_rects, p_rects, threshold=0.5):
             false_positives.append(p)
     return (false_positives, false_negatives, true_positives)
 
+def evaluate_with_groundtruth(tasks, threshold_flowers, threshold_pollinators):
+    '''
+    # Evaluate predictions with manual labels
 
-def main(project_id, user_id, threshold_flowers, threshold_pollinators):
-    # read tasks from minio storage
-    if not os.path.exists('ground_truth/tasks.json'):
-        read_tasks_from_minio()
+    current situation: a predefined set of images was exported from the
+    database as label studio tasks. in those tasks, multiple users labelled
+    flowers and pollinators in the images. the labels are rectangles with
+    classes and confidence values, and optional notes. the labels are used as
+    ground truth data to evaluate the predictions inferred by our model from
+    the same set of images.
 
-    # read tasks from json file
-    tasks_grouped = {}
-    with open('ground_truth/tasks.json', 'r') as f:
-        tasks = json.load(f)
-        # there are multiple objects with the same task id those are labels by
-        # different users. group the objects by task id.
-        for t in tasks:
-            if t['task']['project'] != project_id:
-                continue
-            task_id = t['task']['id']
-            task_user = t['completed_by']['id']
-            if task_id not in tasks_grouped:
-                tasks_grouped[task_id] = {}
-            result = [r for r in t['result'] if r['from_name'] in ['pollinator', 'flower']]
-            tasks_grouped[task_id][task_user] = { 'task': t['task'], 'result': result, 'project': t['project'] }
+    the manual labels are associated to the predictions by finding the
+    prediction with the highest IoU (intersection over union) with the ground
+    truth label. if the IoU is above a certain threshold, then the prediction is
+    considered a true positive. if the IoU is below the threshold, then the
+    prediction is considered a false positive. if there is no prediction for a
+    ground truth label, then the label is considered a false negative.
 
-    # print CSV header
-    print('task_id, file_id, fl_fp, fl_fn, fl_tp, fl_f1, po_fp, po_fn, po_tp, po_f1')
+    the precision, recall and f1 score are calculated for the flowers and
+    pollinators separately.
+    '''
+
+    stats = {
+        'fl_fp': 0, 'fl_fn': 0, 'fl_tp': 0,
+        'po_fp': 0, 'po_fn': 0, 'po_tp': 0
+    }
 
     with pg.connect(host=crd.db.host, port=crd.db.port, database=crd.db.database, user=crd.db.user, password=crd.db.password) as conn:
-        for task in tasks_grouped.values():
-            data = None
-            # each task has results from multiple users, select one
-            if user_id in task:
-                data = task[user_id]
-            else:
-                continue
+        tqdm.write('comparing labels to predictions in database...')
+        for data in tqdm(tasks):
 
             file_id = int(data['task']['data']['file_id'])
 
@@ -209,25 +210,206 @@ def main(project_id, user_id, threshold_flowers, threshold_pollinators):
             fl_fp, fl_fn, fl_tp = match_all(gt_flowers, p_flowers, threshold_flowers)
             po_fp, po_fn, po_tp = match_all(gt_pollinators, p_pollinators, threshold_pollinators)
 
-            # flower scores
-            fl_precision = len(fl_tp) / (len(fl_tp) + len(fl_fp)) if len(fl_tp) + len(fl_fp) != 0 else 1
-            fl_recall = len(fl_tp) / (len(fl_tp) + len(fl_fn)) if len(fl_tp) + len(fl_fn) != 0 else 1
-            fl_f1 = 2 * (fl_precision * fl_recall) / (fl_precision + fl_recall) if fl_precision + fl_recall != 0 else 0
+            # update stats
+            stats['fl_fp'] += len(fl_fp)
+            stats['fl_fn'] += len(fl_fn)
+            stats['fl_tp'] += len(fl_tp)
+            stats['po_fp'] += len(po_fp)
+            stats['po_fn'] += len(po_fn)
+            stats['po_tp'] += len(po_tp)
 
-            # pollinator scores
-            po_precision = len(po_tp) / (len(po_tp) + len(po_fp)) if len(po_tp) + len(po_fp) != 0 else 1
-            po_recall = len(po_tp) / (len(po_tp) + len(po_fn)) if len(po_tp) + len(po_fn) != 0 else 1
-            po_f1 = 2 * (po_precision * po_recall) / (po_precision + po_recall) if po_precision + po_recall != 0 else 0
+    # print stats
+    # flower scores
+    fl_precision = stats['fl_tp'] / (stats['fl_tp'] + stats['fl_fp']) if stats['fl_tp'] + stats['fl_fp'] != 0 else 1
+    fl_recall = stats['fl_tp'] / (stats['fl_tp'] + stats['fl_fn']) if stats['fl_tp'] + stats['fl_fn'] != 0 else 1
+    fl_f1 = 2 * (fl_precision * fl_recall) / (fl_precision + fl_recall) if fl_precision + fl_recall != 0 else 0
+    print('---')
+    print('flower scores')
+    print('precision:', fl_precision)
+    print('recall:', fl_recall)
+    print('f1 score:', fl_f1)
 
-            # print results
-            print(f"{data['task']['id']}, {data['task']['data']['file_id']}, {len(fl_fp)}, {len(fl_fn)}, {len(fl_tp)}, {fl_f1:.3f}, {len(po_fp)}, {len(po_fn)}, {len(po_tp)}, {po_f1:.3f}")
+    # pollinator scores
+    po_precision = stats['po_tp'] / (stats['po_tp'] + stats['po_fp']) if stats['po_tp'] + stats['po_fp'] != 0 else 1
+    po_recall = stats['po_tp'] / (stats['po_tp'] + stats['po_fn']) if stats['po_tp'] + stats['po_fn'] != 0 else 1
+    po_f1 = 2 * (po_precision * po_recall) / (po_precision + po_recall) if po_precision + po_recall != 0 else 0
+    print('---')
+    print('pollinator scores')
+    print('precision:', po_precision)
+    print('recall:', po_recall)
+    print('f1 score:', po_f1)
+
+def evaluate_with_confidence(tasks):
+    '''
+    # Evaluate predictions with manual confidences
+
+    current situation: predictions where labelled as true or false positives,
+    each task has multiple results that are linked by a common id. the results
+    have different types: rectanglelabels, choices and textarea.
+
+    rectanglelabels:    contains the label and the rectangle coordinates
+                        (['value']['rectanglelabels'] = ['wildbiene'])
+    choices:            contains the manually assigned confidence value
+                        (['value']['choices'] = ['false positive'], the choices
+                        being 'false positive', 'high confidence', 'medium
+                        confidence' or 'low confidence')
+    textarea:           contains notes (['value']['text'] = ['this is a note'])
+
+    if the confidence is 'false positive' then the rectangle is a false
+    positive, if the confidence is 'high confidence', 'medium confidence' or
+    'low confidence' then the rectangle is a true positive.
+
+    of all results, calculate the precision. recall and f1 score cannot be
+    calculated without knowing about false negatives (and ideally true
+    negatives). the results are also split by pollinator class.
+    '''
+
+    conditions = []
+    # get tasks by specified user only
+    for task in tasks:
+        results_by_id = {}
+        for result in task['result']:
+            for confidence in task['confidence']:
+                if confidence['id'] == result['id']:
+                    result['test_result'] = confidence['value']['choices'][0]
+                    # condition tuple: (class, confidence)
+                    conditions.append((result['value']['rectanglelabels'][0], confidence['value']['choices'][0]))
+            results_by_id[result['id']] = result
+    true_positives = [r for r in conditions if r[1] in ['high confidence', 'medium confidence', 'low confidence']]
+    false_positives = [r for r in conditions if r[1] == 'false positive']
+
+    # there can be multiple results per task, so the length of the conditions
+    # list is not equal to the number of tasks
+    print(f'of {len(conditions)} predictions: {len(true_positives)} TP, {len(false_positives)} FP')
+    print(f'precision or positive predictive value (PPV): {len(true_positives) / (len(true_positives) + len(false_positives)):.3f}')
+    print('---')
+
+    tp_classes = {}
+    fp_classes = {}
+    for c in conditions:
+        if c[1] in ['high confidence', 'medium confidence', 'low confidence']:
+            if c[0] not in tp_classes:
+                tp_classes[c[0]] = 0
+            tp_classes[c[0]] += 1
+        else:
+            if c[0] not in fp_classes:
+                fp_classes[c[0]] = 0
+            fp_classes[c[0]] += 1
+
+    print('tp', 'fp', 'ppv', 'class', sep='\t')
+    for c in ['fliege', 'honigbiene', 'hummel', 'schwebfliege', 'wildbiene']:
+        precision = tp_classes.get(c, 0) / (tp_classes.get(c, 0) + fp_classes.get(c, 0))
+        print(tp_classes.get(c, 0), fp_classes.get(c, 0), f'{precision:.3f}', c, sep='\t')
+
+def main(mode, project_id, user_id, threshold_flowers, threshold_pollinators):
+    # read tasks from minio storage
+    cache_file = f'ground_truth/labelstudio_tasks.json'
+    if not os.path.exists(cache_file):
+        read_tasks_from_minio(cache_file)
+
+    if mode == 'confidences':
+        origins = ['prediction-changed', 'prediction']
+    if mode == 'labels':
+        origins = ['manual']
+
+    # read tasks from json file
+    tasks_grouped = {}
+    with open(cache_file, 'r') as f:
+        tasks = json.load(f)
+        # there are multiple objects with the same task id those are labels by
+        # different users. group the objects by task id.
+        for t in tasks:
+            if t['task']['project'] != project_id:
+                continue
+            if t['task']['is_labeled'] == False: # tasks without targets that are completed have this set to True.
+                continue
+            if t['was_cancelled'] == True:
+                continue
+
+            task_user = t['completed_by']['id']
+            task_id = t['task']['id']
+            if task_user not in tasks_grouped:
+                tasks_grouped[task_user] = {}
+            if task_id not in tasks_grouped[task_user]:
+                tasks_grouped[task_user][task_id] = {}
+            result = [r for r in t['result'] if r['origin'] in origins and r['from_name'] in ['pollinator', 'flower']]
+            confidence = [r for r in t['result'] if r['origin'] in origins and r['from_name'] == 'confidence']
+            tasks_grouped[task_user][task_id] = { 'task': t['task'], 'result': result, 'confidence': confidence, 'project': t['project'] }
+
+    print('running mode:', mode)
+    print('project id:', project_id)
+    print('user id:', user_id)
+    print(f'found {len(tasks_grouped[user_id])} tasks')
+    if mode == 'labels':
+        print(f'thresholds: flowers = {threshold_flowers}, pollinators = {threshold_pollinators}')
+
+    # count tasks that have confidence labels
+    annotated_rectangles = 0
+    annotated_confidences = 0
+    for task in tasks_grouped[user_id].values():
+        # for specified user and project only
+        if task['project'] == project_id:
+            if len(task['result']) > 0:
+                annotated_rectangles += 1
+            if len(task['confidence']) > 0:
+                annotated_confidences += 1
+            else:
+                # this is ok, in labels mode there can be tasks that don't contain targets
+                if mode == 'confidences':
+                    print(f'no confidence labels for task {task["task"]["id"]}')
+
+    print('---')
+    print(f'found {annotated_rectangles} annotations with predictions')
+    print(f'found {annotated_confidences} annotations with confidence labels')
+
+    # if tasks_with_rectangles and tasks_with_confidence are not equal, then the
+    # set is incomplete and the evaluation cannot be performed
+    if annotated_rectangles != annotated_confidences:
+        print('incomplete set, cannot perform evaluation')
+        sys.exit(1)
+
+    # evaluate predictions with manual confidences
+    if mode == 'confidences':
+        # fold annotations of two users into one
+        # other user_ids present in the dataset
+        other_users = list(filter(lambda x: x != user_id, tasks_grouped.keys()))
+        other_tasks = []
+        if len(other_users) > 0:
+            # get tasks unique to the first user
+            unique_task_ids = set(tasks_grouped[other_users[0]].keys()) - set(tasks_grouped[user_id].keys())
+            print(f'found {len(unique_task_ids)} tasks unique to another user, adding them to the evaluation...')
+            other_tasks = [tasks_grouped[other_users[0]][task_id] for task_id in unique_task_ids]
+        evaluate_with_confidence(list(tasks_grouped[user_id].values()) + other_tasks)
+
+    # evaluate predictions with manual labels
+    if mode == 'labels':
+        evaluate_with_groundtruth(tasks_grouped[user_id].values(), threshold_flowers, threshold_pollinators)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Evaluate predictions with ground truth data.')
-    parser.add_argument('--project_id', type=int, help='Label Studio Project ID', default=5)
-    parser.add_argument('--user_id', type=int, help='Label Studio User ID', default=5)
-    parser.add_argument('--threshold_flowers', type=float, help='IoU threshold for matching flowers (0...1)', default=0.4)
-    parser.add_argument('--threshold_pollinators', type=float, help='IoU threshold for matching pollinators (0...1)', default=0.3)
+    parser.add_argument('mode', type=str, help='Mode: Compare predictions with manual labels or manual confidences', choices=['labels', 'confidences'])
+    parser.add_argument('-p', '--project_id', type=int, help='Label Studio Project ID', default=5)
+    parser.add_argument('-u', '--user_id', type=int, help='Label Studio User ID', default=5)
+    parser.add_argument('-tf', '--threshold_flowers', type=float, help='IoU threshold for matching flowers (0...1)', default=0.4)
+    parser.add_argument('-tp', '--threshold_pollinators', type=float, help='IoU threshold for matching pollinators (0...1)', default=0.3)
     args = parser.parse_args()
 
-    main(args.project_id, args.user_id, args.threshold_flowers, args.threshold_pollinators)
+    '''
+    # Examples
+
+    ## Evaluate predictions with manual labels
+
+    Compare ground-truth labels of project 5, user 5,
+    with predictions in the database.
+
+    `python evaluate.py labels -p 5 -u 5 -tf 0.4 -tp 0.3`
+
+    ## Evaluate predictions with manual confidences
+
+    Evaluate predictions of project 10, user 5,
+    with the manual confidences.
+
+    `python evaluate.py confidences -p 10 -u 5`
+    '''
+
+    main(args.mode, args.project_id, args.user_id, args.threshold_flowers, args.threshold_pollinators)
